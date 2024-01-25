8ffbeb789a1b96aeface7f53c8bea55264fb45c3
Jp Keys <jpmusik29@gmail.com>
2024-01-25 14:53:17 -0500

    Project setup

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import './styles.css';
+import quests from './questsArray.js'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Statistics Review</h1>
+      <Question />
+    </div>
+  )
+}
+
+// function Questionnaire() {
+//   return (
+//     <div>
+//       <Question />
+//     </div>
+//   )
+// }
+
+function Question() {
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ src/questsArray.js
@@ -0,0 +1,13 @@
+const quests = [
+    {
+        title : "What are the 4 steps of the statistical analysis?",
+        text : ''
+    },
+    {
+        title : "What are the types of research questions?",
+        text : 'Cause-and-effect'
+    },
+
+];
+
+export default quests;
