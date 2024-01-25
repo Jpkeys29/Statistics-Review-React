e9a74665a8517c0e5e142c3f8980c941674b7597
Jp Keys <jpmusik29@gmail.com>
2024-01-25 15:12:13 -0500

    git test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import quests from './questsArray.js'
 export default function App() {
   return (
     <div>
-      <h1>Statistics Review</h1>
+      <h1>Statistics Review Question</h1>
       <Question />
     </div>
   )
@@ -21,6 +21,9 @@ export default function App() {
 function Question() {
   return (
     <div>
+      <img
+      src='https://images.unsplash.com/photo-1584308666744-24d5c474f2ae?q=80&w=2860&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      />
 
     </div>
   )
