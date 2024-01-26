0138f84f88a38809ac7f089d9c07d18b8af7d187
Jp Keys <jpmusik29@gmail.com>
2024-01-26 07:13:47 -0500

    Passing image props

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-// import './styles.css';
+import './styles.css';
 import quests from './questsArray.js'
 
 export default function App() {
   return (
-    <div>
+    <div className='questions'>
       <h1>Statistics Review Question</h1>
-      <Question />
+      <Question 
+        adorn={{
+          // src:'https://images.unsplash.com/photo-1557683316-973673baf926?q=80&w=2858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          width:1000, backgroundColor:'blue', height:500
+        }}    
+      />
+      <Question 
+        adorn={{
+          // src:'https://images.unsplash.com/photo-1531685250784-7569952593d2?q=80&w=2748&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          width:1000, backgroundColor:'red', height:500
+        }}    
+      />
+      <Question 
+        adorn={{
+          // src:'https://images.unsplash.com/photo-1522441815192-d9f04eb0615c?q=80&w=2727&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          width:1000, backgroundColor:'yellow', height:500
+        }}    
+      />
     </div>
   )
 }
 
-// function Questionnaire() {
-//   return (
-//     <div>
-//       <Question />
-//     </div>
-//   )
-// }
-
-function Question() {
+function Question({adorn}) {
+  // console.log(adorn)
   return (
-    <div>
+    <div className='questions'>
       <img
-      src='https://images.unsplash.com/photo-1584308666744-24d5c474f2ae?q=80&w=2860&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      // src={adorn.src}
+      style={{width:adorn.width,
+      backgroundColor:adorn.backgroundColor,
+      height:adorn.height}}
       />
-
     </div>
   )
 }
