b145eb5941713716109dac0fd50ee4193cd75542
Jp Keys <jpmusik29@gmail.com>
2024-01-29 11:07:30 -0500

    Rendering list trial 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './styles.css';
-import quests from './questsArray.js'
+import {quests} from './questsArray.js'
 
 export default function App() {
-  const listQuests = quests.map(quest => <li key={quest.id} >{quest.title} {quest.text} </li>)
   return (
-    <div className='questions'>
-      <ul>{listQuests}</ul>
-    </div>
+    <>
+      {quests.map(function(data, index) {
+        return(
+          <div className="questions" key={data.id}>
+            Question title :  {data.title}
+          </div>
+        )
+      })}
+    </>
   )
-  }
+}
+
+// function Question (){
+//   return(
+//     <div className='questions'>
+
+//     </div>
+//   )
+// }
+// const listQuests = quests.map(quest => <li key={quest.id} >{quest.title} {quest.text} </li>)
+// return (
+//   <div className='questions'>
+//     <ul>{listQuests}</ul>
+//   </div>
+// )
