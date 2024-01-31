d0757cd390d6317c975799429dbf1f676ae1083a
Jp Keys <jpmusik29@gmail.com>
2024-01-31 12:34:41 -0500

    Numbering fix

--- src/question.js
+++ src/question.js
@@ -6,7 +6,7 @@ export default function Question ({num, title, answer}){
         <div className='questions' >
             {quests.map((ele, index) => (
                 <div key={index} className='item' >
-                <p className='number'>{index}</p> 
+                <p className='number'>{ele.id}</p> 
                 <p className='title' >{ele.title}</p>
                 <p className='icon' >+</p>
                 <div className='content-box' >{ele.answer}</div>
