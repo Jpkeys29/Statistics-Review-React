dd808a8c515f3e587818135d52ea917c39af7362
Jp Keys <jpmusik29@gmail.com>
2024-02-02 14:08:48 -0500

    Update answer toggle state

--- src/question.js
+++ src/question.js
@@ -4,10 +4,15 @@ import { useState } from 'react';
 
 export default function Question ({num, title, answer}){
     const [isOpen, setIsOpen] = useState(false);
+
+    function handleToggle(){
+        setIsOpen((isOpen) => !isOpen);
+    }
+
     return (   
         <div className='questions'>
             {quests.map((ele, index) => (
-                <div key={index} className='item' >
+                <div key={index} className='item' onClick={handleToggle}>
                 <p className='number'>{ele.id}</p> 
                 <p className='title' >{ele.title}</p>
                 <p className='icon'>{isOpen ? "-" : "+"}</p>
