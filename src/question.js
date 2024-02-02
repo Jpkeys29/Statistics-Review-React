371b6529a35ade524125ad74a36540cb983103fb
Jp Keys <jpmusik29@gmail.com>
2024-02-02 12:13:31 -0500

    Answers conditional rendering

--- src/question.js
+++ src/question.js
@@ -1,18 +1,18 @@
+import Button from './button.js';
 import {quests} from './questsArray.js'
 import { useState } from 'react';
 
 export default function Question ({num, title, answer}){
     const [isOpen, setIsOpen] = useState(false);
-    return (
-        
-        <div className='questions' >
+    return (   
+        <div className='questions'>
             {quests.map((ele, index) => (
                 <div key={index} className='item' >
                 <p className='number'>{ele.id}</p> 
                 <p className='title' >{ele.title}</p>
-                <p className='icon' >-</p>
-                <div className='content-box' >{ele.answer}</div>
-                </div>
+                <p className='icon' >+</p>
+                {isOpen && <div className='content-box' >{ele.answer}</div>}
+                </div>   
             ))}
         </div>          
     );    
