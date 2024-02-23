cec9c8e50e5233e2124c0dcfdb734ae774d7deac
Jp Keys <jpmusik29@gmail.com>
2024-02-23 10:10:25 -0500

    Passing question object to buttons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './styles.css';
-import {quests} from './questsArray.js'
+import {DESCRIPT, quests} from './questsArray.js'
 import Question from './question.js';
 import QuestionList from './QuestionList.js'
 
 export default function App() {
-  function handleToggle (){
-    console.log('Funciona!');
+
+  function handleToggle (selectedQuestions){
+    console.log(selectedQuestions);
   } 
   return (
     <div>
-      <Question preguntas={quests}/>
+      <Question preguntas={quests} />
+      {/* <Question preguntas={DESCRIPT} /> */}
+  
       <section className='sidebar'>
         <h2>Topics</h2>
         <menu>
-          <QuestionList onClick={handleToggle}>Experiment Design</QuestionList>
-          <QuestionList onClick={handleToggle}>Descriptive Statistics</QuestionList>
+          <QuestionList preguntas={quests} onClick={() => handleToggle(quests)}>Experiment Design</QuestionList>
+          <QuestionList preguntas={DESCRIPT} onClick={() => handleToggle(DESCRIPT)}>Descriptive Statistics</QuestionList>
           <QuestionList onClick={handleToggle}>Introduction To Probability</QuestionList>
           <QuestionList onClick={handleToggle}>Rules of Probability</QuestionList>
         </menu>
--- src/questsArray.js
+++ src/questsArray.js
@@ -21,3 +21,26 @@ export const quests = [
     },
 
 ];
+
+export const DESCRIPT =[
+    {
+        id : 1,
+        title : 'What is the measure of variability respective to the mean?',
+        answer : 'The standard deviation'
+    },
+    {
+        id : 2,
+        title : 'What are the most common types of graph distributions?',
+        answer : 'Skewed right, skewd left, and normal'
+    },
+    {
+        id : 3,
+        title : 'In a skewed right distribution, what is the relationship between the mean and the median',
+        answer : 'The mean is higher than the median'
+    },
+    {
+        id : 4,
+        title : 'Why the median is often used to describe salaries?',
+        answer : 'The median is resistant, so outliers will not distort the value of the center'
+    },
+]
